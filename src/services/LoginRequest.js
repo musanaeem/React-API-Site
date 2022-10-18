1784f108a96c5d3607be84e75a4c54040ebaec2f
Musa Naeem <musa.naeem@arbisoft.com>
2022-10-18 13:04:54 +0500

    changed url

--- src/services/LoginRequest.js
+++ src/services/LoginRequest.js
@@ -24,9 +24,9 @@ async function LoginRequest(credentials) {
 
         const env = process.env;
 
-        //const url = `${env.REACT_APP_HOST}:${env.REACT_APP_PORT}${env.REACT_APP_LOGIN_PATH}`
+        const url = `${env.REACT_APP_HOST}:${env.REACT_APP_PORT}${env.REACT_APP_LOGIN_PATH}`
 
-        return fetch('http://localhost:8000/api/login/', options)
+        return fetch(url, options)
         .then(response => response.json())
         .then(data => {
           return data;
