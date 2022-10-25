39ff6fabc331949972705108e49188ccbdb25184
musanaeem <musa.naeem@arbisoft.com>
2022-10-25 18:05:02 +0500

    added === to ProtectedRoutes

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -4,6 +4,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 export default class ProtectedRoutes extends Component {
 
   render() {
-    return (this.props.isLoggedIn == true) ? <Outlet/> : <Navigate to='/login'/>;
+    return (this.props.isLoggedIn === true) ? <Outlet/> : <Navigate to='/login'/>;
   }
 }
