87135383da1c6d5b3970c972be0a0f2bd5d596b9
musanaeem <musa.naeem@arbisoft.com>
2022-10-25 16:38:07 +0500

    ESlint comment added

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -139,6 +139,7 @@ const RegisterForm = (props) => {
     
 
     const isValidEmail = (emailValue) => {
+        // eslint-disable-next-line no-useless-escape
         const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     
         if(!emailRegex.test(emailValue)){
