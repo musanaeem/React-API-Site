c4fd232e5e8b09d3dca5fa538e641844c7d0c7f0
musanaeem <musa.naeem@arbisoft.com>
2022-10-27 13:52:05 +0500

    changed delete css

--- src/pages/DeleteRecord.js
+++ src/pages/DeleteRecord.js
@@ -37,16 +37,16 @@ const DeleteRecord = (props) => {
 //Make one component with conditional props
   return (
     <div className='deletePage'>
-        <p> Are you sure you want to delete <span>"{state.username}"</span>? </p>
+        <p className='delete-message'> Are you sure you want to delete <span className='delete-username'>"{state.username}"</span>? </p>
 
-        <form>
+        <form className='delete-form'>
 
             {props.type === 'Bio' ? 
                 <Link to='/bio'> Cancel </Link> :
                 <Link to='/blog'> Cancel </Link>
             }
 
-            <input className="submit" type="submit" name="Confirm" value="Confirm" onClick={deleteRecord}/>
+            <input className="submit delete-request" type="submit" name="Confirm" value="Confirm" onClick={deleteRecord}/>
 
         </form>
     </div>
