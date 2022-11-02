fb49c6a36a372266d4930b09e88818ddd43e46cd
musanaeem <musa.naeem@arbisoft.com>
2022-11-02 11:19:14 +0500

    Updated tests to include username, email and password

--- src/components/__tests__/RegisterFormTest.js
+++ src/components/__tests__/RegisterFormTest.js
@@ -26,6 +26,55 @@ describe('Name Validation', () => {
     })
 })
 
+describe('Username Validation', () => {
+
+    test('validate Username field being present', () => {
+        render(<RegisterForm/>)
+
+        const usernameInput = screen.getByPlaceholderText('Username...')
+        expect(usernameInput).toBeInTheDocument();
+        expect(usernameInput).toHaveAttribute('type', 'text')
+        fireEvent.change(usernameInput, {'target': {'value': 'TestUserame45'}})
+        expect(usernameInput).toHaveAttribute('value','TestUserame45')
+    })
+})
+
+describe('Email Validation', () => {
+
+    test('validate Email field being present', () => {
+        render(<RegisterForm/>)
+
+        const emailInput = screen.getByPlaceholderText('Email...')
+        expect(emailInput).toBeInTheDocument();
+        expect(emailInput).toHaveAttribute('type', 'text')
+        fireEvent.change(emailInput, {'target': {'value': 'Testemail@random.com'}})
+        expect(emailInput).toHaveAttribute('value','Testemail@random.com')
+    })
+})
+
+describe('Passwords Validation', () => {
+
+    test('validate Password field being present', () => {
+        render(<RegisterForm/>)
+
+        const passwordInput = screen.getByPlaceholderText('Password...')
+        expect(passwordInput).toBeInTheDocument();
+        expect(passwordInput).toHaveAttribute('type', 'password')
+        fireEvent.change(passwordInput, {'target': {'value': 'pass123@'}})
+        expect(passwordInput).toHaveAttribute('value','pass123@')
+    })
+
+    test('validate Password Confirmation field being present', () => {
+        render(<RegisterForm/>)
+
+        const passwordConfirmationInput = screen.getByPlaceholderText('Password Confirmation...')
+        expect(passwordConfirmationInput).toBeInTheDocument();
+        expect(passwordConfirmationInput).toHaveAttribute('type', 'password')
+        fireEvent.change(passwordConfirmationInput, {'target': {'value': 'pass123@'}})
+        expect(passwordConfirmationInput).toHaveAttribute('value','pass123@')
+    })
+})
+
 test('Validate submit button is disabled on form render', () => {
     render(<RegisterForm/>)
 
